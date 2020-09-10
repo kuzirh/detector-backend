86457b3eb68be1523355dd92f290ff9f6dd8dfc3
kuzirh <hrizukmichael@gmail.com>
2020-09-09 21:21:45 -0400

    working through issue where Image is not being zip and deployed to layer

--- ocr/handler.py
+++ ocr/handler.py
@@ -1,12 +1,26 @@
+import base64
+import io
 import json
+import os
+
 import pytesseract
 from PIL import Image
 
+if os.getenv('AWS_EXECUTION_ENV') is not None:
+    os.environ['LD_LIBRARY_PATH'] = '/opt/lib'
+    os.environ['TESSDATA_PREFIX'] = '/opt/tessdata'
+    pytesseract.pytesseract.tesseract_cmd = '/opt/tesseract'
 
-def hello(event, context):
+
+def ocr(event, context):
+    print(event)
+    request_body = json.loads(event['body'])
+    image = io.BytesIO(base64.b64decode(request_body['image']))
+
+    text = pytesseract.image_to_string(Image.open(image))
 
     body = {
-        "text": pytesseract.image_to_string(Image.open('test.jpg')),
+        "text": text
     }
 
     response = {
